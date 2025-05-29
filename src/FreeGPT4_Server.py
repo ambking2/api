85786ede4aec45bcf48ee87747d33a28279ddc63
ambking2 <ambking2209@gmail.com>
2025-05-29 15:14:25 +0330

    Update FreeGPT4_Server.py

--- src/FreeGPT4_Server.py
+++ src/FreeGPT4_Server.py
@@ -564,11 +564,3 @@ async def get_token():
 if __name__ == "__main__":
     # Starts the server, change the port if needed
     app.run("0.0.0.0", port=args.port, debug=False)
-
-from flask import jsonify
-
-@app.route("/", methods=["GET"])
-def root():
-    return jsonify({
-        "routes": [str(rule) for rule in app.url_map.iter_rules()]
-    })
